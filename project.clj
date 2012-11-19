525ab4dcf2e702dcf96703d3d0702e99f5238bba
Vadim Geshel <vadim@yummly.com>
2012-11-19 09:29:11 -0800

    'First commit.'

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.vgeshel/ssh-wagon "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true
+  :dependencies [[org.apache.maven.wagon/wagon-ssh-external "1.0-beta-7"]
+                 [org.apache.maven.wagon/wagon-ssh "1.0-beta-7"]])
---
+++ src/leiningen/wagons.clj
@@ -0,0 +1,2 @@
+{"scp" #(org.apache.maven.wagon.providers.ssh.jsch.ScpWagon.)
+ "scpexe" #(org.apache.maven.wagon.providers.ssh.external.ScpExternalWagon.)}
